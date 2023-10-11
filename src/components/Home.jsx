dd31f42b0edd5987a463970d0fb7fc188338cfb6
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-11 12:40:37 -0700

    updating dispatch in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import productList from '../data/productList.json'
+import cartSlice from '../data/cartSlice'
+import { useDispatch } from 'react-redux'
 import '../styles/home.scss'
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const { addToCart, removeFromCart, clearAllItems} = cartSlice.actions 
+
   return (
     <div className="container product-catalogue">
       <div className="row">
@@ -15,7 +20,9 @@ const Home = () => {
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  <button className="btn btn-primary">Add to cart</button>
+                  <button className="btn btn-primary" onClick={()=> dispatch(addToCart)}>Add To Cart</button>
+                  <button className="btn btn-primary" onClick={()=> dispatch(removeFromCart)}>Remove From Cart</button>
+
                 </div>
               </div>
             </div>
