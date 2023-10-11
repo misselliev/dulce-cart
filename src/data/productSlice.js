27dfec6a236c3492e146fd1463f5eb567d02fdc7
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-11 14:30:46 -0700

    adding productSlice, fetching local api

---
+++ src/data/productSlice.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const fetchAllProducts = createAsyncThunk('fetch-all-products', async(apiUrl) => {
+    const response = await fetch(apiUrl)
+    return response.json()
+})
+
+const productSlice = createSlice({
+    name: 'products',
+    initialState:{ data: [], fetchStatus: ''},
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
+            state.data = action.payload
+            state.fetchStatus = 'success'
+        }).addCase(fetchAllProducts.pending, (state) => {
+            state.fetchStatus = 'pending'
+        }).addCase(fetchAllProducts.rejected, (state) => {
+            state.fetchStatus = 'error'
+        })
+    }
+})
+
+export default productSlice
